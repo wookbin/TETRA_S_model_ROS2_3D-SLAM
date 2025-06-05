e757ab671b79f91b97ef2b083ad8896416b74c03
Wookbin Jin <58063370+wookbin@users.noreply.github.com>
2025-06-05 11:38:48 +0900

    Add files via upload

---
+++ tetra_navigation2/launch/tetra_navigation.launch.py
@@ -0,0 +1,94 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    pkg_bme_ros2_navigation = get_package_share_directory('tetra_navigation2')
+
+    sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time', default_value='False',
+        description='Flag to enable use_sim_time'
+    )
+
+    map_name_arg = DeclareLaunchArgument(
+        'map_name', default_value='office.yaml',
+        description='Name of the map file to use'
+    )
+
+
+    # Path to the Slam Toolbox launch file
+    nav2_localization_launch_path = os.path.join(
+        get_package_share_directory('tetra_navigation2'),
+        'launch',
+        'localization_launch.py'
+    )
+
+    nav2_navigation_launch_path = os.path.join(
+        get_package_share_directory('tetra_navigation2'),
+        'launch',
+        'navigation_launch.py'
+    )
+
+    localization_params_path = os.path.join(
+        get_package_share_directory('tetra_navigation2'),
+        'params',
+        'amcl_localization.yaml'
+    )
+
+    navigation_params_path = os.path.join(
+        get_package_share_directory('tetra_navigation2'),
+        'params',
+        'navigation.yaml'
+    )
+
+    virtual_wall_launch_path = os.path.join(
+        get_package_share_directory('virtual_wall'),
+        'launch',
+        'virtual_wall.launch.py'
+    )
+
+    #map_file_path = PathJoinSubstitution([
+    #    pkg_bme_ros2_navigation, 'maps', LaunchConfiguration('map_name')
+    #])
+    
+    # Dynamically constructed paths
+    map_file_path = PathJoinSubstitution([
+        '/home/tetra/ros2_ws/src/tetra_navigation2', 'maps', LaunchConfiguration('map_name')
+    ])
+
+    localization_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(nav2_localization_launch_path),
+        launch_arguments={
+                'use_sim_time': LaunchConfiguration('use_sim_time'),
+                'params_file': localization_params_path,
+                'map': map_file_path,
+        }.items()
+    )
+
+    navigation_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(nav2_navigation_launch_path),
+        launch_arguments={
+                'use_sim_time': LaunchConfiguration('use_sim_time'),
+                'params_file': navigation_params_path,
+                'map': map_file_path,
+        }.items()
+    )
+
+    virtual_wall_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(virtual_wall_launch_path),
+        launch_arguments={}.items()
+    )
+
+    launchDescriptionObject = LaunchDescription()
+    launchDescriptionObject.add_action(sim_time_arg)
+    launchDescriptionObject.add_action(localization_launch)
+    launchDescriptionObject.add_action(navigation_launch)
+    launchDescriptionObject.add_action(virtual_wall_launch)
+
+    return launchDescriptionObject
