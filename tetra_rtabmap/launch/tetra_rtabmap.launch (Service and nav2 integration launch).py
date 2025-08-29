04eca9db501751032e38cd2a69c81da7b9e67b86
Wookbin Jin <58063370+wookbin@users.noreply.github.com>
2025-08-29 16:01:43 +0900

    Update tetra_rtabmap.launch (Service and nav2 integration launch).py

--- tetra_rtabmap/launch/tetra_rtabmap.launch (Service and nav2 integration launch).py
+++ tetra_rtabmap/launch/tetra_rtabmap.launch (Service and nav2 integration launch).py
@@ -75,7 +75,7 @@ def launch_setup(context, *args, **kwargs):
         'Mem/STMSize', '30',
         'Mem/LaserScanNormalK', '20',
         'Optimizer/Strategy','1', # 1: g2o / 2: GTSAM 
-        'Icp/VoxelSize', '0.5',
+        'Icp/VoxelSize', '0.15', # 0.5
         'Icp/PointToPlaneK', '20',
         'Icp/PointToPlaneRadius', '0',
         'Icp/PointToPlane', 'true',
@@ -116,6 +116,8 @@ def launch_setup(context, *args, **kwargs):
             'use_sim_time': use_sim_time,
             'Mem/IncrementalMemory': incremental_memory,
             'Mem/InitWMWithAllNodes': initwmwithallnodes,
+            'Mem/BinDataKept': 'false', # option add..
+            'Mem/ReduceGraph': 'true', # option add..
             'Grid/FromScan': 'true',
             'Grid/RayTracing': 'true',
             'Grid/3D': 'true',
@@ -125,7 +127,7 @@ def launch_setup(context, *args, **kwargs):
             'Grid/NormalsSegmentation': 'false',
             'Grid/CellSize': '0.05',
             'database_path': db_path,
-            'Optimizer/Strategy': '1', # add..
+            'Optimizer/Strategy': '1',
             
         }],
         #arguments=rtabmap_arguments,
