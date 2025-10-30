04661f89ed6cfe02c41fe7c62d6f5e291da51302
Wookbin Jin <58063370+wookbin@users.noreply.github.com>
2025-10-30 13:48:02 +0900

    Update tetra_rtabmap_nav2.launch.py

--- tetra_rtabmap/launch/tetra_rtabmap_nav2.launch.py
+++ tetra_rtabmap/launch/tetra_rtabmap_nav2.launch.py
@@ -6,7 +6,7 @@ from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 import os
-import time  # ← 추가
+import time
 import rclpy
 from rclpy.node import Node as RclpyNode
 from rtabmap_msgs.srv import PublishMap
@@ -15,7 +15,6 @@ from rtabmap_msgs.srv import PublishMap
 def wait_and_publish_then_start_nav(context, *args, **kwargs):
     use_sim_time_str = LaunchConfiguration('use_sim_time').perform(context).lower()
 
-    # 1) PublishMap 호출만 수행 (RViz는 이미 실행됨)
     rclpy.init()
     node = RclpyNode('publish_map_client')
     client = node.create_client(PublishMap, '/rtabmap/publish_map')
@@ -29,18 +28,15 @@ def wait_and_publish_then_start_nav(context, *args, **kwargs):
             node.get_logger().error('Timeout waiting for /rtabmap/publish_map')
             node.destroy_node()
             rclpy.shutdown()
-            return []  # 실패 시 이후 액션 생략
+            return []
 
     req = PublishMap.Request()
     req.global_map = True
     req.optimized = True
     req.graph_only = False
 
-    #future = client.call_async(req)
-    #rclpy.spin_until_future_complete(node, future)
-    
     future = client.call_async(req)
-    end_by = time.monotonic() + 180.0  # 최대 180초 대기
+    end_by = time.monotonic() + 180.0
     while rclpy.ok() and not future.done():
         rclpy.spin_once(node, timeout_sec=0.1)
         if time.monotonic() > end_by:
@@ -59,7 +55,6 @@ def wait_and_publish_then_start_nav(context, *args, **kwargs):
     node.destroy_node()
     rclpy.shutdown()
 
-    # 2) 퍼블리시 성공 후 Nav2 포함
     include_nav = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(
@@ -92,7 +87,6 @@ def launch_setup(context, *args, **kwargs):
         'Mem/IncrementalMemory', 'false',
         'Mem/InitWMWithAllNodes', 'true',
         'Optimizer/Strategy', '1',
-        #'Optimizer/Robust', 'true',
         'Icp/VoxelSize', '0.1',
         'Icp/PointToPlaneK', '25',
         'Icp/PointToPlaneRadius', '0',
@@ -106,46 +100,60 @@ def launch_setup(context, *args, **kwargs):
         'Icp/Strategy', '1',
         'Icp/OutlierRatio', '0.12', #'0.05'
         'Icp/CorrespondenceRatio', '0.12',
-        'Icp/RangeMin', '0.05',
+        'Icp/RangeMin', '0.1',
         'Icp/RangeMax', '25.0',
         'Vis/MaxFeatures', '0',
         'Vis/MinInliers', '0',
-        # 비전 완전 OFF(경고 줄이기)
         'Kp/DetectorStrategy', '0',
-        # 근접/로컬 루프 매칭(= map→odom 보정 촘촘히)
-        'Rtabmap/DetectionRate', '10.0',         # 5.0 → 3.0 (보정 빈도 ↑, 과도한 CPU 방지)
+        'Rtabmap/DetectionRate', '10.0',
         'RGBD/LinearUpdate', '0.08',
         'RGBD/AngularUpdate', '0.04',
         'RGBD/ProximityBySpace', 'true',
         'RGBD/ProximityByTime', 'false', 
         'RGBD/ProximityAngle', '180',
-        'RGBD/ProximityMaxGraphDepth', '0',     # 0 전체 그래프에서 후보 탐색
-        'RGBD/ProximityPathMaxNeighbors', '1', #'1'
+        'RGBD/ProximityMaxGraphDepth', '0',
+        'RGBD/ProximityPathMaxNeighbors', '1',
         'RGBD/NeighborLinkRefining', 'true',
         'RGBD/LocalLoopDetectionSpace', 'true',
         'RGBD/LocalLoopDetectionTime', 'false', #true
         'RGBD/LocalLoopDetectionRadius', '6.0',
-        'RGBD/StartAtOrigin', 'true', # mapping mode: false || localization mode: true
+        'RGBD/StartAtOrigin', 'true',
         'RGBD/MaxOdomCacheSize', '5',
     ])
 
 
-    # 1) rtabmap 먼저
+    rtabmap_util = Node(
+        package='rtabmap_util',
+        executable='point_cloud_assembler',
+        output='screen',
+        parameters=[{
+            'assembling_time': 0.05,
+            'fixed_frame_id': 'base_footprint',
+            'use_sim_time': use_sim_time,
+        }],
+        remappings=[
+        ('cloud', lidar_topic),
+        ]
+    )
+    
     rtabmap_slam = Node(
         package='rtabmap_slam',
         executable='rtabmap',
         output='screen',
         parameters=[{
+            'publish_tf': True,
+            'map_frame_id': 'map',
+            'odom_frame_id': 'odom',
             'frame_id': 'base_footprint',
             'subscribe_depth': False,
             'subscribe_rgb': False,
             'subscribe_scan': False,
             'subscribe_scan_cloud': True,
-            'approx_sync': True,
-            'topic_queue_size': 100,         # 동기화 유연성
-            'sync_queue_size': 100,          # 동기화 유연성
-            'queue_size': 100,               # 동기화 유연성
-            'wait_for_transform': 0.5, #0.3
+            'approx_sync': False, #True
+            'topic_queue_size': 100,
+            'sync_queue_size': 100,
+            'queue_size': 100,
+            'wait_for_transform': 0.5,
             'use_sim_time': use_sim_time,
             'Mem/BinDataKept': 'false',
             'Mem/ReduceGraph': 'true',
@@ -153,7 +161,7 @@ def launch_setup(context, *args, **kwargs):
             'Grid/RayTracing': 'true',
             'Grid/3D': 'false',
             'Grid/RangeMax': '25.0',
-            'Grid/RangeMin': '0.05',
+            'Grid/RangeMin': '0.1',
             'Grid/MaxObstacleHeight': '2.0',
             'Grid/MaxGroundHeight': '0.5',
             'Grid/NormalsSegmentation': 'false',
@@ -163,12 +171,11 @@ def launch_setup(context, *args, **kwargs):
         }],
         arguments=rtabmap_arguments + ['--ros-args', '--log-level', 'WARN'],
         remappings=[
-            ('scan_cloud', lidar_topic),
+            ('scan_cloud', 'assembled_cloud'),
             ('odom', '/odometry/filtered'),
         ],
     )
 
-    # 2) RViz를 먼저 띄움 (퍼블리시보다 먼저 구독 시작)
     rviz_config_file = 'tetra_3d_navigation2.rviz'
     rviz_config_dir = os.path.join(
         get_package_share_directory('tetra_rtabmap'),
@@ -184,13 +191,12 @@ def launch_setup(context, *args, **kwargs):
         output='screen'
     )
 
-    # 3) 잠깐 대기 후 PublishMap 호출 → 성공 시 Nav2 포함
     publish_then_nav = TimerAction(
         period=2.0,
         actions=[OpaqueFunction(function=wait_and_publish_then_start_nav)]
     )
 
-    return [rtabmap_slam, rviz_node, publish_then_nav]
+    return [rtabmap_util, rtabmap_slam, rviz_node, publish_then_nav]
 
 
 def generate_launch_description():
