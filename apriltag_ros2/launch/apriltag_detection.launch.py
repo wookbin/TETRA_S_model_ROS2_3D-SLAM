1db5ed21aa3187c45f32f209a203d86035a57187
Wookbin Jin <58063370+wookbin@users.noreply.github.com>
2025-10-31 09:33:16 +0900

    Update apriltag_detection.launch.py

--- apriltag_ros2/launch/apriltag_detection.launch.py
+++ apriltag_ros2/launch/apriltag_detection.launch.py
@@ -9,7 +9,7 @@ def generate_launch_description():
             name='apriltag_node',
             output='screen',
             parameters=[
-                {'camera_frame': 'camera1_color_frame'}, #'camera'
+                {'camera_frame': 'camera'}, #'camera1_depth_optical_frame'
                 {'tag_family': 'tag36h11'},
                 {'tag_size': 0.1}  # Adjust to your tag's size in meters
             ],
@@ -19,4 +19,3 @@ def generate_launch_description():
             ],
         ),
     ])
-
